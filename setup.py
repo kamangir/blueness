22961ece241f07f360adc39e747c09882fc3bbfc
kamangir <arash@kamangir.net>
2024-05-26 10:02:54 -0700

    build refactor - kamangir/bolt#746

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from blueness import NAME, VERSION, DESCRIPTION
+from blueness import NAME, VERSION, DESCRIPTION, REPO_NAME
 from blueness.pypi import setup
 
 
 setup(
     filename=__file__,
-    repo_name="blueness",
+    repo_name=REPO_NAME,
     name=NAME,
     version=VERSION,
     description=DESCRIPTION,
