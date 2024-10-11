a96b94283243363c389961b2bc9d3c7eaad7cfcd
kamangir <arash@kamangir.net>
2024-10-10 17:54:36 -0700

    blueness upgrade - kamangir/bolt#746

--- blueness/.abcli/blueness.sh
+++ blueness/.abcli/blueness.sh
@@ -4,7 +4,20 @@ function blueness() {
     local task=$(abcli_unpack_keyword $1 help)
 
     if [ $task == "help" ]; then
+        blueness upgrade "$@"
         blueness version
+        return
+    fi
+
+    if [ $task == "upgrade" ]; then
+        local module
+        for module in blueness \
+            blue_options \
+            blue_objects \
+            blue_geo \
+            notebooks_and_scripts; do
+            pip install --upgrade $module
+        done
         return
     fi
 
