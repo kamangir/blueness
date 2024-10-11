d7e477eebd6dda3c922b39574817fb575b47d7bd
kamangir <arash@kamangir.net>
2024-10-10 19:16:04 -0700

    blueness upgrade - kamangir/bolt#746

--- blueness/.abcli/blueness.sh
+++ blueness/.abcli/blueness.sh
@@ -3,13 +3,18 @@
 function blueness() {
     local task=$(abcli_unpack_keyword $1 help)
 
-    if [ $task == "help" ]; then
+    if [[ "$task" == "help" ]]; then
         blueness upgrade "$@"
-        blueness version
         return
     fi
 
     if [ $task == "upgrade" ]; then
+        if [[ "$2" == "help" ]]; then
+            abcli_show_usage "blueness upgrade" \
+                "upgrade blueness and its modules 🌀."
+            return
+        fi
+
         local module
         for module in blueness \
             blue_options \
--- blueness/__init__.py
+++ blueness/__init__.py
@@ -4,6 +4,6 @@ ICON = "🌀"
 
 DESCRIPTION = f"{ICON} the blueness of ai."
 
-VERSION = "3.77.1"
+VERSION = "3.78.1"
 
 REPO_NAME = "blueness"
