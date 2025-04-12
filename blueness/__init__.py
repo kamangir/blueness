2ea9459371949112a639ff1a63ac51ce540db1cd
kamangir <arash@kamangir.net>
2025-04-12 11:42:52 -0700

    start of refactors-2025-04-12-zjs2fv 🌀 - kamangir/bolt#746

--- blueness/__init__.py
+++ blueness/__init__.py
@@ -4,6 +4,10 @@ ICON = "🌀"
 
 DESCRIPTION = f"{ICON} the blueness of ai."
 
-VERSION = "3.115.1"
+VERSION = "3.116.1"
 
 REPO_NAME = "blueness"
+
+MARQUEE = (
+    "https://github.com/kamangir/assets/raw/main/blue-objects/marquee.png?raw=true"
+)
